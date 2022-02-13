d2eecd23e258e953df63a763a4538968a1605fcf
nojasm <jonas.mertens@posteo.de>
2022-02-13 01:13:05 +0100

    Painfully added a mouse selection feature

--- src/js/skriptus.js
+++ src/js/skriptus.js
@@ -1,5 +1,5 @@
 const ipcRenderer = require("electron").ipcRenderer;
-const { genGUID, insertAt, getTypeFromClass, getChildIndex } = require("./js/utils.js");
+const { genGUID, insertAt, getTypeFromClass, getChildIndex, rectsDoOverlap } = require("./js/utils.js");
 const { contextMenuOpen, contextMenuClose } = require("./js/contextMenu.js");
 
 
@@ -154,27 +154,66 @@ function skriptContentDeleteUndefined() {
 	});
 }
 
-function changeTypeFromEvent(event, newType) {
-	skript.content[getChildIndex(event.target)].type = newType;
+function changeTypeFromIndex(index, newType) {
+	skript.content[index].type = newType;
 	renderSkript();
 }
 
+function changeTypeFromEvent(event, newType) {
+	changeTypeFromIndex(getChildIndex(event.target), newType);
+}
+
 function deleteElementFromEvent(event) {
-	delete skript.content[getChildIndex(event.target)];
+	let index = getChildIndex(event.target);
+
+	delete skript.content[index];
 	skriptContentDeleteUndefined();
 
 	renderSkript();
 }
 
 function duplicateElementFromEvent(event) {
-	let elIndex = getChildIndex(event.target);
-	let el = skript.content[elIndex];
+	let index = getChildIndex(event.target);
+	let el = skript.content[index];
+
 	skript.content = insertAt(skript.content, {
 		type: el.type,
 		text: el.text
-	}, elIndex + 1);
+	}, index + 1);
 
-	_focusedContentIndex = elIndex + 1;
+	_focusedContentIndex = index + 1;
+
+	renderSkript();
+}
+
+function deleteSelected() {
+	let newContent = [];
+	skript.content.forEach((el, i) => {
+		if (!selectedIndices.includes(i)) {
+			newContent.push(el);
+		}
+	});
+
+	skript.content = newContent;
+
+	renderSkript();
+}
+
+function duplicateSelected() {
+	// console.log("Insert", getSelectedElements(), "at index", selectedIndices[selectedIndices.length - 1]);
+	if (skript.content.length == selectedIndices.length) {
+		// Add
+		getSelectedElements().forEach((el, _) => {
+			skript.content.push(el);
+		})
+	} else {
+		// Insert
+		skript.content = insertAt(
+			skript.content,
+			getSelectedElements(),
+			selectedIndices[selectedIndices.length - 1] + 1
+		);
+	}
 
 	renderSkript();
 }
@@ -194,6 +233,185 @@ var _focusedContentIndex = null;
 var contextMenu = document.getElementById("context-menu");
 var contextMenuIsOpen = false;
 
+var selectionEl = document.getElementById("selection");
+var selectionCtx = selectionEl.getContext("2d");
+var selectionStart = [null, null];
+var isSelecting = false;
+var selectedIndices = [];
+
+var main = document.getElementById("main");
+
+function resizeSelectionElementToMax() {
+	selectionEl.style.width = document.body.clientWidth + "px";
+	selectionEl.style.height = document.body.clientHeight + "px";
+	selectionEl.width = document.body.clientWidth;
+	selectionEl.height = document.body.clientHeight;
+}
+
+resizeSelectionElementToMax();
+
+window.addEventListener("resize", function() {
+	resizeSelectionElementToMax();
+});
+
+function startSelection(x, y) {
+	isSelecting = true;
+	selectionStart = [x, y];
+}
+
+function updateSelection() {
+	// Remove "undefined" items
+	selectedIndices = selectedIndices.filter((value, index, array) => {
+		return value != undefined;
+	});
+
+	skript.content.forEach((el, i) => {
+		let domEl = document.getElementsByClassName("skript__line")[i];
+		if (selectedIndices.includes(i)) {
+			domEl.classList.add("selection__selected");
+		} else {
+			domEl.classList.remove("selection__selected");
+		}
+	});
+}
+
+function isBetween(a, b, val) {
+	a = Math.abs(a);
+	b = Math.abs(b);
+
+	if (a > b) {
+		return a > val && val > b;
+	} else if (b > a) {
+		return b > val && val > a;
+	}
+}
+
+function setSelectionBox(pos1, pos2) {
+	// Put box on ctx here
+	selectionCtx.fillStyle = "#77befd33";
+	selectionCtx.clearRect(0, 0, selectionEl.width, selectionEl.height);
+	selectionCtx.fillRect(pos1[0], pos1[1], pos2[0] - pos1[0], pos2[1] - pos1[1]);
+	//selectionCtx.fillRect(0, 0, 50, 50);
+
+	// Now mark everything between selectionStart and [x, y]
+	skript.content.forEach((el, i) => {
+		// Check if element is in selection
+		let domEl = document.getElementsByClassName("skript__line")[i];
+		let rect = domEl.getBoundingClientRect();
+		let r = {};
+		r.x = pos1[0]; r.y = pos1[1]; r.width = pos2[0] - pos1[0]; r.height = pos2[1] - pos1[1];
+
+		if (r.height < 0) {
+			r.y += r.height;
+			r.height = Math.abs(r.height);
+		}
+
+		if (r.width < 0) {
+			r.x += r.width;
+			r.width = Math.abs(r.width);
+		}
+
+		if (rectsDoOverlap(r, rect)) {
+			if (!selectedIndices.includes(i))
+				selectedIndices.push(i);
+
+		} else {
+			if (selectedIndices.indexOf(i) >= 0)
+				delete selectedIndices[selectedIndices.indexOf(i)];
+		}
+	});
+
+	selectedIndices = selectedIndices.sort();
+
+	updateSelection();
+}
+
+function endSelection(x, y) {
+	isSelecting = false;
+	selectionStart = [null, null];
+
+	selectionCtx.clearRect(0, 0, selectionEl.width, selectionEl.height);
+
+	// Select elements
+}
+
+function clearSelection() {
+	selectionCtx.clearRect(0, 0, selectionEl.width, selectionEl.height);
+
+	selectedIndices = [];
+	isSelecting = false;
+
+	skript.content.forEach((el, i) => {
+		let domEl = document.getElementsByClassName("skript__line")[i];
+		if (domEl.classList.contains("selection__selected"))
+			domEl.classList.remove("selection__selected");
+	});
+}
+
+function getSelectedElements() {
+	// Use filter here
+	let se = [];
+	skript.content.forEach((item, i) => {
+		if (selectedIndices.includes(i))
+			se.push(item);
+	})
+
+	return se;
+}
+
+function getPrettyNameFromType(name) {
+	let _name = "?";
+	elements.forEach((el, i) => {
+		if (el.name == name) _name = el.prettyName;
+	});
+
+	return _name;
+}
+
+function getCMInfoTextFromSelections() {
+	// If all of the selected elements are of the same type, this
+	// function will return this type. If not, it will return a "*" instead.
+
+	let _type = null;
+	getSelectedElements().forEach((el, i) => {
+		// Use getPrettyNameFromType(el.type) here
+		if (_type == null) _type = el.type;
+		else if (_type != el.type) _type = "*";
+	});
+
+	if (_type != "*")
+		_type = getPrettyNameFromType(_type);
+
+	return _type + " (" + selectedIndices.length + ")"
+}
+
+document.body.addEventListener("mousedown", function(event) {
+	if ((event.target.id != "context-menu" && event.target.parentElement.id != "context-menu" && event.target.parentElement.parentElement.id != "context-menu") &&
+		event.button == 0 && selectedIndices.length > 0) {
+		clearSelection();
+
+	} else {
+		if (event.target.id == "main" || event.target.id == "skript") {
+			contextMenuClose();
+			startSelection(event.pageX, event.pageY);
+		}
+	}
+
+});
+
+document.body.addEventListener("mouseup", function(event) {
+	if (isSelecting) {
+		endSelection(event.pageX, event.pageY);
+	}
+});
+
+document.body.addEventListener("mousemove", function(event) {
+	if (isSelecting) {
+		setSelectionBox(selectionStart, [event.pageX, event.pageY])
+	}
+});
+
+
 
 document.body.addEventListener("click", function(event) {
 	if (event.target.id != "context-menu" && event.target.parentElement.id != "context-menu") {
@@ -203,18 +421,33 @@ document.body.addEventListener("click", function(event) {
 
 document.body.addEventListener("contextmenu", function(event) {
 	if (event.target.classList.contains("skript__line")) {
-		contextMenuOpen(event.clientX, event.clientY, [
-			{ label: getTypeFromClass(event.target.classList[1]), type: "info"},
-			{ type: "seperator"},
-			{ label: "Change to", type: "category", children: elements.map((i) => { return {
-				label: i.prettyName,
-				color: "#6e7544",
-				callback: () => {changeTypeFromEvent(event, i.name)}
-			}})},
-			{ type: "seperator"},
-			{ label: "Duplicate", color: "#3d7882", callback: () => {duplicateElementFromEvent(event)} },
-			{ label: "Delete", color: "#8c3232", callback: () => {deleteElementFromEvent(event)} }
-		]);
+		if (selectedIndices.length <= 1) {
+			contextMenuOpen(event.clientX, event.clientY, [
+				{ label: getTypeFromClass(event.target.classList[1]), type: "info"},
+				{ type: "seperator"},
+				{ label: "Change to", type: "category", children: elements.map((i) => { return {
+					label: i.prettyName,
+					color: "#6e7544",
+					callback: () => {changeTypeFromEvent(event, i.name)}
+				}})},
+				{ type: "seperator"},
+				{ label: "Duplicate", color: "#3d7882", callback: () => {duplicateElementFromEvent(event)} },
+				{ label: "Delete", color: "#8c3232", callback: () => {deleteElementFromEvent(event)} }
+			]);
+		} else {
+			contextMenuOpen(event.clientX, event.clientY, [
+				{ label: getCMInfoTextFromSelections(), type: "info"},
+				{ type: "seperator"},
+				{ label: "Change to", type: "category", children: elements.map((i) => { return {
+					label: i.prettyName,
+					color: "#6e7544",
+					callback: () => {selectedIndices.forEach((ix, _) => {changeTypeFromIndex(ix, i.name)}); clearSelection()}
+				}})},
+				{ type: "seperator"},
+				{label: "Duplicate", color: "#3d7882", callback: () => {duplicateSelected(); clearSelection()} },
+				{ label: "Delete", color: "#8c3232", callback: () => {deleteSelected(); clearSelection()} }
+			]);
+		}
 	} else {
 		contextMenuClose();
 	}
--- src/js/utils.js
+++ src/js/utils.js
@@ -12,8 +12,13 @@ exports.genGUID = function () {
 exports.insertAt = function (haystack, needle, index) {
 	let newHaystack = [];
 	haystack.forEach((item, i) => {
-		if (i == index)
-			newHaystack.push(needle);
+		if (i == index) {
+			if (needle instanceof Object) {
+				needle.forEach((ni, _) => {newHaystack.push(ni)})
+			} else {
+				newHaystack.push(needle);
+			}
+		}
 
 		newHaystack.push(item);
 	});
@@ -28,4 +33,18 @@ exports.getTypeFromClass = function (type) {
 
 exports.getChildIndex = function (el) {
 	return [...el.parentElement.children].indexOf(el);
+}
+
+function valueInRange(val, min, max) {
+	return (val >= min) && (val <= max);
+}
+
+exports.rectsDoOverlap = function (rect1, rect2) {
+	let xOverlap = valueInRange(rect1.x, rect2.x, rect2.x + rect2.width) ||
+	               valueInRange(rect2.x, rect1.x, rect1.x + rect1.width);
+
+	let yOverlap = valueInRange(rect1.y, rect2.y, rect2.y + rect2.height) ||
+	               valueInRange(rect2.y, rect1.y, rect1.y + rect1.height);
+
+	return (xOverlap && yOverlap);
 }
