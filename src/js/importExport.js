dec279a57a9dafd486a3ddd6ae97a53bbd4e4d1d
nojasm <sanohd@posteo.de>
2022-12-18 18:17:54 +0100

    Started adding other import options

--- src/js/importExport.js
+++ src/js/importExport.js
@@ -1,26 +1,49 @@
+function fountainImporter(content) {}
+
+function trelbyImporter(content) {
+
+}
+
 exports.importSkript = function() {
 	smallWindow.empty();
 	smallWindow.setTitle("Import");
 	smallWindow.show();
 
-	let optionFountain = document.createElement("div");
-	let optionFountainIcon = document.createElement("img");
-	let optionFountainTitle = document.createElement("p");
+	let importOptions = [
+		{
+			"icon": "../res/fountain-sign-32.png",
+			"name": "Fountain",
+			"function": (content) => {
+				return fountainImporter(content)
+			}
+		},
+		{
+			"icon": "../res/fountain-sign-32.png",
+			"name": "Trelby",
+			"function": (content) => {
+				return trelbyImporter(content)
+			}
+		}
+	]
 
-	optionFountain.classList.add("import-export__option");
-	optionFountainIcon.classList.add("import-export__option__icon");
-	optionFountainTitle.classList.add("import-export__option__title");
+	importOptions.forEach((importer) => {
+		let importerDiv = document.createElement("div");
+		let importerIcon = document.createElement("img");
+		let importerTitle = document.createElement("p");
 
-	optionFountainIcon.src = "../res/fountain-sign-32.png";
-	optionFountainTitle.innerHTML = "Fountain";
+		importerDiv.classList.add("import-export__option");
+		importerIcon.classList.add("import-export__option__icon");
+		importerTitle.classList.add("import-export__option__title");
 
-	optionFountain.addEventListener("click", function(event) {
+		importerIcon.src = importer.icon;
+		importerTitle.innerText = importer.name;
+		importerIcon.addEventListener("click", importer.function);
 
+		importerDiv.appendChild(importerIcon);
+		importerDiv.appendChild(importerTitle);
+
+		smallWindow.body.appendChild(importerDiv)
 	});
-
-	optionFountain.appendChild(optionFountainIcon);
-	optionFountain.appendChild(optionFountainTitle);
-	smallWindow.body.appendChild(optionFountain);
 }
 
 exports.exportSkript = function() {
